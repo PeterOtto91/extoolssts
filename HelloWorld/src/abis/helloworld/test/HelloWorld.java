d15b2ce217c902572d490d5a928336e324ff8d3d
PeterOtto91 <peter.otto@telenet.be>
2020-09-28 10:39:14 +0200

    hello

---
+++ HelloWorld/src/abis/helloworld/test/HelloWorld.java
@@ -0,0 +1,11 @@
+package abis.helloworld.test;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello " + args[0]);
+
+	}
+
+}
